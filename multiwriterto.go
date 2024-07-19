ba1458b7ba0f3d15db94d4d6f72fad966cff990f
Alon Krymgand <me@alon.kr>
2024-07-19 17:55:54 +0300

    Added documentation

--- multiwriterto.go
+++ multiwriterto.go
@@ -6,10 +6,20 @@ type multiWriterTo struct {
 	writerTos []io.WriterTo
 }
 
+// Implements the io.WriterTo interface.
+//
+// WriteTo writes data from all of the provided writers to w, one after another
+// until all data has been written or an error has occurred. The return value n
+// is the number of total bytes written. If an error has been encountered during
+// the write, it is also returned.
 func MultiWriterTo(writerTos ...io.WriterTo) io.WriterTo {
 	return multiWriterTo{writerTos: writerTos}
 }
 
+// writes data from all of the provided writers to w, one after another
+// until all data has been written or an error has occurred.
+// The return value n is the number of total bytes written.
+// If an error has been encountered during the write, it is also returned.
 func (multiWriter multiWriterTo) WriteTo(destWriter io.Writer) (n int64, err error) {
 	var k int64
 
